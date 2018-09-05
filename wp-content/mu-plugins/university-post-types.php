555a57d6632c7d96cebf329ef44a72a9f9d78812
Mark Simpson <markyz1989@gmail.com>
2018-09-05 09:04:53 +0100

    Section 9 - custom fields, events post type done

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,9 @@
 <?php
 
 function university_post_types() {
+
+	// Event Post Type
+
 	register_post_type('event', array(
 		'supports' => array('title','editor','excerpt'),
 		'has_archive' => true,
@@ -17,6 +20,26 @@ function university_post_types() {
 			'singular_name' => 'Events',
 		),
 		'menu_icon' => 'dashicons-calendar-alt'
+	));
+
+	// Program Post Type
+
+	register_post_type('program', array(
+		'supports' => array('title','editor'),
+		'has_archive' => true,
+		'shown_in_nav_menus' => true,
+		'rewrite' => array(
+			'slug' => 'programs'
+		),
+		'public' => true,
+		'labels' => array(
+			'name' => 'Programs',
+			'add_new_item' => 'Add New Program',
+			'edit_item' => 'Edit Program',
+			'all_items' => 'All Programs',
+			'singular_name' => 'Programs',
+		),
+		'menu_icon' => 'dashicons-awards'
 	));
 }
 
